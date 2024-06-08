3f9566c041ad0449d84edf5e5daab9bcefbb4210
yjbae3974 <brianbae6@gmail.com>
2024-06-09 04:56:26 +0900

    .

--- mzproject/mzapp/models.py
+++ mzproject/mzapp/models.py
@@ -12,19 +12,7 @@ class Question(models.Model):
         return self.quiz
 
 class User(models.Model):
-    name = models.CharField(max_length=255)  # 사용자의 이름
-    start_time = models.DateTimeField()  # 퀴즈 시작 시간
-    total_count = models.IntegerField()  # 총 문제 개수
-    correct_count = models.IntegerField()  # 맞춘 문제 개수
-    end_time = models.DateTimeField()  # 퀴즈 종료 시간
+    user_count = models.IntegerField(default=1)  # 사용자 수
 
     def __str__(self):
-        return self.name
-
-
-class Reward(models.Model):
-    name = models.CharField(max_length=255)  # 보상 이름
-    image = models.ImageField(upload_to='images/')  # 보상 이미지
-    
-    def __str__(self):
-        return self.name
+        return str(self.user_count)
