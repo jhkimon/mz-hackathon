56a4c56b4e4832e1055a6db6bc77048ece10ba0c
yjbae3974 <brianbae6@gmail.com>
2024-06-08 15:16:56 +0900

    뷰 구현 완료

--- mzproject/mzproject/settings.py
+++ mzproject/mzproject/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -27,6 +28,9 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+# MEDIA_URL과 MEDIA_ROOT 설정
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Application definition
 
